69f2677ff180c493ccd7338d002814f9107c3261
Shreyas <shreyas4144@gmail.com>
2025-10-20 15:09:41 +0530

    firebase-update

--- firebase/client.ts
+++ firebase/client.ts
@@ -12,7 +12,7 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: "G-1R7J8KJLMS"
+  measurementId: "G-FM5DJ9DFKZ"
 };
 
 // Initialize Firebase
